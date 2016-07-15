56d90ec487a39678da4c51dcc4d721206e2fbde6
Maldon <maldonmeehan@gmail.com>
2016-07-15 13:31:59 -0700

    add failing test for returns the name of a given stylist

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -7,3 +7,10 @@ describe(Stylist) do
     end
   end
 end
+
+describe('#name') do
+    it('returns the name of a given stylist') do
+      stylist1 = Stylist.new({:id => nil, :name => 'David Mallet'})
+      expect(stylist1.title()).to(eq('David Mallet'))
+    end
+  end
