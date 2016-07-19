45a737fb6c3aa161bf9570d5e1091b14c52a6c62
Maldon <maldonmeehan@gmail.com>
2016-07-18 18:36:20 -0700

    add passing test for deletes a stylist from the database

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -27,14 +27,19 @@ define_method(:initialize) do |attributes|
   end
 
   define_singleton_method(:find) do |id|
-  found_stylist = nil
-  Stylist.all().each do |stylist|
-    if stylist.id().==(id)
-      found_stylist = stylist
+    found_stylist = nil
+    Stylist.all().each do |stylist|
+      if stylist.id().==(id)
+        found_stylist = stylist
+      end
     end
+    found_stylist
   end
-  found_stylist
-end
+
+  define_method(:delete) do
+    DB.exec("DELETE FROM stylists WHERE id=#{self.id()};")
+  end
+
 
 
 end
