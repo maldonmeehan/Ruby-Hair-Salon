e368f03093d097d4d304c6ff740c09b5ea62a343
Maldon <maldonmeehan@gmail.com>
2016-07-18 19:25:08 -0700

    add failing test for returns a client by its id

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -18,9 +18,24 @@ describe(Client) do
 
   describe('#save') do
      it('saves a new client to the database') do
-       test_client = Client.new({:id => nil, :name => "Rey", :stylist_id => 1})
+       test_client = Client.new({:id => nil, :name => "Joanne Ward", :stylist_id => 1})
        test_client.save()
        expect(Client.all()).to(eq([test_client]))
      end
    end
+
+  describe('.find') do
+    it('returns a client by its id') do
+      test_client1 = Client.new({:id => nil, :name => "Joanne Ward", :stylist_id => 1})
+      test_client2 = Client.new({:id => nil, :name => "Laura White", :stylist_id => 1})
+      test_client1.save()
+      test_client2.save()
+      expect(Client.find(test_client2.id())).to(eq(test_client2))
+    end
+  end
+
+
+
+
+
 end
