367d287db314cccec75e4a1728478b48b2699700
Maldon <maldonmeehan@gmail.com>
2016-07-18 20:31:23 -0700

    add failing test spec to adds a stylist to the list of stylists

--- spec/intergration_spec.rb
+++ spec/intergration_spec.rb
@@ -3,10 +3,20 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('the add a stylist route', {:type => :feature}) do
+describe('the enter a stylist route', {:type => :feature}) do
   it('takes the owner to a page where they entered stylist') do
     visit('/')
     click_link('Stylists')
     expect(page). to have_content('Add a stylist')
+  end
+end
+
+describe('the add stylist route', {:type => :feature}) do
+  it('adds a stylist to the list of stylists') do
+    visit('/')
+    click_link('Stylists')
+    fill_in('stylist-name', :with => 'Petra Flannery')
+    click_button('Add Stylist')
+    expect(page). to have_content('Petra Flannery')
   end
 end
