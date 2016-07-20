0d83f72abf44e45f3367c31b673c7463f7384759
maldon <maldonmeehan@gmail.com>
2016-07-19 17:38:11 -0700

    add passing test for add a new stylist

--- app.rb
+++ app.rb
@@ -10,3 +10,20 @@ DB = PG.connect({:dbname => 'hair_salon_test'})
 get("/") do
   erb(:index)
 end
+
+get("/stylists") do
+  @stylests = Stylist.all()
+  erb(:stylists)
+end
+
+get("/stylists/new") do
+  erb(:stylist_form)
+end
+
+post("/stylists") do
+  name = params.fetch("name")
+  stylist = Stylist.new({:id => nil, :name => name})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
--- spec/intergration_spec.rb
+++ spec/intergration_spec.rb
@@ -7,6 +7,16 @@ require('launchy')
 describe('home page path', {:type => :feature}) do
   it('allows a user to view the homepage') do
     visit('/')
-    expect(page).to have_content('Welcome Ruby Hair Salon')
+    expect(page).to have_content('Welcome')
+  end
+end
+
+describe('add a new stylist', {:type => :feature}) do
+  it('allows the user to add a stylist') do
+    visit('/')
+    click_link('Add new stylist')
+    fill_in("name", :with => 'David Mallet')
+    click_button('Add stylist')
+    expect(page).to have_content('David Mallet')
   end
 end
