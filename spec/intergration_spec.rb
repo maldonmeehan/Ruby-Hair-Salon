91a93163575796bb0bd0f90ca638e1f650d83537
maldon <maldonmeehan@gmail.com>
2016-07-19 17:03:00 -0700

    add failing test for home page path

--- spec/intergration_spec.rb
+++ spec/intergration_spec.rb
@@ -2,3 +2,11 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+require('launchy')
+
+describe('home page path', {:type => :feature}) do
+  it('allows a user to view the homepage') do
+    visit('/')
+    expect(page).to have_content('Welcome Ruby Hair Salon')
+  end
+end
