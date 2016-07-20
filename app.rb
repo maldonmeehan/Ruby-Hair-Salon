fee7db969a97875689fa9a2f1f3c2dce068426ef
Maldon <maldonmeehan@gmail.com>
2016-07-20 16:21:07 -0700

    add passing spec for add a new stylist

--- app.rb
+++ app.rb
@@ -12,25 +12,90 @@ get("/") do
 end
 
 get("/stylists") do
-  @stylests = Stylist.all()
+  @stylists = Stylist.all()
   erb(:stylists)
 end
 
 get("/stylists/new") do
+
   erb(:stylist_form)
 end
 
 post("/stylists") do
   name = params.fetch("name")
-  stylist = Stylist.new({:name => name, :id => nil})
-  stylist.save()
+  @stylist = Stylist.new({:name => name, :id => nil})
+  @stylist.save()
   @stylists = Stylist.all()
   erb(:stylists)
 end
 
-get('/stylists/:id') do
-  @stylists = Stylist.all()
-  @stylists = Stylist.find(params.fetch("id").to_i())
-  @clients = Client.all()
-  erb(:stylist)
-end
+# get('/surveys/:id') do
+#   @survey = Survey.find(params.fetch("id").to_i())
+#   erb(:survey)
+# end
+#
+# get('/questions') do
+#   @questions = Question.all()
+#   erb(:questions)
+# end
+#
+# get('/questions/new') do
+#   @surveys = Survey.all()
+#   erb(:question_form)
+# end
+#
+# post('/questions') do
+#   description = params.fetch('description')
+#   survey_id = params.fetch('survey_id').to_i()
+#   @survey = Survey.find(survey_id)
+#   @question = Question.create({:description => description, :survey_id => survey_id, :id => nil})
+#   @questions = Question.all()
+#   if @question.save()
+#     erb(:questions)
+#   else
+#     erb(:errors)
+#   end
+# end
+#
+# get('/questions/:id') do
+#   @question = Question.find(params.fetch('id').to_i())
+#   erb(:question_edit)
+# end
+#
+# get('/questions/:id/edit') do
+#   @question = Question.find(params.fetch('id').to_i())
+#   erb(:question_edit)
+# end
+#
+# patch('/questions/:id') do
+#   description = params.fetch("description")
+#   @question = Question.find(params.fetch("id").to_i())
+#   @question.update({:description => description})
+#   erb(:question_edit)
+# end
+#
+# delete('/questions/:id/delete') do
+#   @question = Question.find(params.fetch('id').to_i())
+#   @question.delete()
+#   @questions = Question.all()
+#   erb(:questions)
+# end
+#
+# get('/surveys/:id/edit') do
+#   @survey = Survey.find(params.fetch("id").to_i())
+#   erb(:survey_edit)
+# end
+#
+# patch('/surveys/:id/edit') do
+#   name = params.fetch('name')
+#   @survey = Survey.find(params.fetch("id").to_i())
+#   @survey.update({:name => name})
+#   erb(:survey)
+# end
+#
+# delete('/surveys/:id/delete') do
+#   @survey = Survey.find(params.fetch('id').to_i())
+#   @survey.delete()
+#   @surveys = Survey.all()
+#   erb(:surveys)
+# end
--- spec/intergration_spec.rb
+++ spec/intergration_spec.rb
@@ -20,30 +20,30 @@ describe('add a new stylist', {:type => :feature}) do
     expect(page).to have_content('David Mallet')
   end
 end
-
-# describe('view single stylist instance route', {:type => :feature}) do
-#   it('allows the user to view a single stylist') do
+#
+# # describe('view single stylist instance route', {:type => :feature}) do
+# #   it('allows the user to view a single stylist') do
+# #     visit('/')
+# #     click_link('Add new stylist')
+# #     fill_in("name", :with => 'David Mallet')
+# #     click_button('Add stylist')
+# #     expect(page).to have_content('David Mallet')
+# #     click_link("David Mallet")
+# #     expect(page).to have_content('Individual stylist page')
+# #   end
+# # end
+#
+# describe('update a stylists name route', {:type => :feature}) do
+#   it('allows the user to update the stylists name') do
 #     visit('/')
 #     click_link('Add new stylist')
 #     fill_in("name", :with => 'David Mallet')
 #     click_button('Add stylist')
 #     expect(page).to have_content('David Mallet')
-#     click_link("David Mallet")
-#     expect(page).to have_content('Individual stylist page')
+#     click_link('David Mallet')
+#     click_link("Update")
+#     fill_in('name', :with => 'John Mallet')
+#     click_button('Update')
+#     expect(page).to have_content('John Mallet')
 #   end
 # end
-
-describe('update a stylists name route', {:type => :feature}) do
-  it('allows the user to update the stylists name') do
-    visit('/')
-    click_link('Add new stylist')
-    fill_in("name", :with => 'David Mallet')
-    click_button('Add stylist')
-    expect(page).to have_content('David Mallet')
-    click_link('David Mallet')
-    click_link("Update")
-    fill_in('name', :with => 'John Mallet')
-    click_button('Update')
-    expect(page).to have_content('John Mallet')
-  end
-end
